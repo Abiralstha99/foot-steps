cd04377b5f6e97cfe61574538e476535b236cbab
Tejaswi Amatya <tejuamatya@gmail.com>
2026-02-14 13:58:24 +0100

    fixing issue with url

--- backend/routes/photoRoutes.ts
+++ backend/routes/photoRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { updatePhoto } from "../controllers/photoController";
+import { getAllPhotos, updatePhoto, getPhotoById } from "../controllers/photoController";
 import { clerkAuth, syncUser } from "../middleware/auth";
+
 
 const photoRouter = express.Router();
 
 photoRouter.patch("/:photoId", clerkAuth, syncUser, updatePhoto);
-
+photoRouter.get("/all", clerkAuth, syncUser, getAllPhotos);
+photoRouter.get("/:photoId", clerkAuth, syncUser, getPhotoById);
 export default photoRouter;
